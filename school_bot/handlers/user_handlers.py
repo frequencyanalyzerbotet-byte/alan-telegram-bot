9d62dce63723fd346f3a0e75d271258a8d184807
frequencyanalyzerbotet-byte <frequencyanalyzerbotet@gmail.com>
2025-11-18 15:32:45 +0500

    Delete school_bot directory

--- school_bot/handlers/user_handlers.py
+++
@@ -1,5 +0,0 @@
-from views.keyboards import main_menu
-from views.templates import MAIN_MENU
-
-async def start(update, context):
-    await update.message.reply_text(MAIN_MENU, reply_markup=main_menu())
